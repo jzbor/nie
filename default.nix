ae52dad950e52f8bcc19c4eed1574710a3986d97
jzbor <jzbor@posteo.net>
2026-03-26 22:32:09 +0100

    Add OCI image

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@
   system ? builtins.currentSystem,
   pins ? import ./npins,
   pkgs ? import pins.nixpkgs { inherit system; },
+  docker-nixpkgs ? pins.docker-nixpkgs { inherit pkgs; },
 }:
 let
   craneLib = import (pins.crane + /default.nix) { inherit pkgs; };
@@ -23,34 +24,75 @@ let
     strictDeps = true;
     inherit cargoArtifacts;
   };
+
+  buildImageWithNix = import ("${docker-nixpkgs}" + "/images/nix/default.nix");
 in rec {
-  packages.default = craneLib.buildPackage (commonArgs // {
-    nativeBuildInputs = with pkgs; [
-      makeWrapper
-      installShellFiles
-    ];
-    postFixup = ''
-      wrapProgram $out/bin/nie \
-        --prefix PATH ${pkgs.lib.makeBinPath [ pkgs.nix ]}
-    '';
-    postInstall = ''
-      mkdir ./manpages ./completions
-      $out/bin/nie man ./manpages
-      $out/bin/nie completions ./completions
-      installManPage ./manpages/*
-      installShellCompletion completions/nie.{bash,fish,zsh}
-    '';
-  });
-  packages.npins = pkgs.npins;
+  packages = {
+    inherit (pkgs) npins;
+
+    default = packages.nie;
+    nie = craneLib.buildPackage (commonArgs // {
+      nativeBuildInputs = with pkgs; [
+        makeWrapper
+        installShellFiles
+      ];
+      postFixup = ''
+        wrapProgram $out/bin/nie \
+          --prefix PATH ${pkgs.lib.makeBinPath [ pkgs.nix ]}
+      '';
+      postInstall = ''
+        mkdir ./manpages ./completions
+        $out/bin/nie man ./manpages
+        $out/bin/nie completions ./completions
+        installManPage ./manpages/*
+        installShellCompletion completions/nie.{bash,fish,zsh}
+      '';
+    });
+
+    nixImage = buildImageWithNix {
+      inherit (pkgs) dockerTools bashInteractive cacert coreutils curl gnutar gzip iana-etc nix openssh xz;
+
+      # We are actually going to use Git so we use the full version.
+      gitReallyMinimal = pkgs.git;
+    };
+
+    nieImage = pkgs.dockerTools.buildImage {
+      inherit (packages.default) name;
+      tag = "latest";
+
+      fromImage = packages.nixImage;
+      copyToRoot = pkgs.buildEnv {
+        name = "image-root";
+        paths = with pkgs; [
+          packages.nie
+          bash
+        ];
+        pathsToLink = [ "/bin" ];
+      };
+
+      config.Cmd = [ "/bin/bash" ];
+    };
+  };
 
   checks = {
     default = checks.package;
     package = packages.default;
+
     clippy = craneLib.cargoClippy (commonArgs //{
       inherit cargoArtifacts;
       cargoClippyExtraArgs = "--all-targets -- --deny warnings";
     });
+
     statix = cfLib.mkStatixCheck { src = ./.; };
+
     deadnix = cfLib.mkDeadnixCheck { src = ./.; };
+  };
+
+  devShells.default = pkgs.mkShellNoCC {
+    name = packages.default.name;
+    packages = with pkgs; [
+      npins
+      nix-prefetch-docker
+    ];
   };
 }
